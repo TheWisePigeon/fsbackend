47084a2d52a754f4b498231621de88ef639cf790
TheWisePigeon <zozozozeph@gmail.com>
2022-09-21 11:22:39 +0000

    refactor: updated product schema

--- src/models/product.ts
+++ src/models/product.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const productSchema = new mongoose.Schema({
     name: String,
     price: Number,
+    image: Number,
     description: String,
     ingredients: String
 })
